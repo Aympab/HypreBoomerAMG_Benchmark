9c86a6400153a5a679db2465a26704b0946241b7
Aymeric MILLAN <aympab@gmail.com>
2022-01-13 21:36:59 +0100

    profiling OK

--- log/log_parser.sh
+++ log/log_parser.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 # rm all_logs_Dense seq_logs_Dense.csv MPI_logs_Dense.csv
 # rm all_logs_Sparse seq_logs_Sparse.csv MPI_logs_Sparse.csv
-rm weak_logs.csv
 
-echo "np,n,time,ite,resi" >> weak_logs.csv
-for np in 1 2 4 8 16 32 64 128
-do
-  for n in 128 256 512 1024 2048 4096 8192 16384
-  do
-    time=$(grep "took" log-emilia-NPI${np} | cut -d " " -f3)
-    ite=$(grep "Iterations =" log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
-    resi=$(grep "Norm =" log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
+filename=weak/weak_start256.csv
+rm $filename
 
-    echo "${np},${n},${time},${ite},${resi}" >> weak_logs.csv
-  done
+echo "np,nx,time,ite,resi" >> $filename
+
+#array=( 1 2 4 8 16 28 32 56 64 84 )
+#array2=( 100 200 400 800 1600 2800 3200 5600 6400 8400)
+
+#array=( 1 2 4 8 16 28 32 56 64 84 )
+#array2=( 33 66 132 264 528 924 1056 1848 2112 2772)
+
+array=( 1 2 4 8 16 28 32 56 64 84 )
+array2=( 256 512 1024 2048 4096 7168 8192 14336 16752 21504)
+
+for i in "${!array[@]}"; do
+  np=${array[i]}
+  nx=${array2[i]}
+
+  time=$(grep "took" weak/np${np}_nx${nx}.logs | cut -d " " -f3)
+  ite=$(grep "Iterations =" weak/np${np}_nx${nx}.logs | cut -d "=" -f2 | cut -d " " -f2)
+  resi=$(grep "Norm =" weak/np${np}_nx${nx}.logs | cut -d "=" -f2 | cut -d " " -f2)
+
+  echo "${np},${nx},${time},${ite},${resi}" >> $filename
 done
 
 echo "Done !"
