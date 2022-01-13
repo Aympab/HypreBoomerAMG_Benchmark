c11d0ee32291c327c56cd7ad478af5267add7079
Aymeric MILLAN <aympab@gmail.com>
2022-01-13 20:46:02 +0100

    profiling

--- log/log_parser.sh
+++ log/log_parser.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # rm all_logs_Dense seq_logs_Dense.csv MPI_logs_Dense.csv
 # rm all_logs_Sparse seq_logs_Sparse.csv MPI_logs_Sparse.csv
-rm emilia_strong_logs.csv
+rm weak_logs.csv
 
-echo "np,time,ite,resi" >> emilia_strong_logs.csv
-for np in 1 2 4 6 8 12 16 20 24 28 32 36 40 48 56
+echo "np,n,time,ite,resi" >> weak_logs.csv
+for np in 1 2 4 8 16 32 64 128
 do
-  time=$(grep "took" log-emilia-NPI${np} | cut -d " " -f3)
-  ite=$(grep "Iterations =" log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
-  resi=$(grep "Norm =" log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
+  for n in 128 256 512 1024 2048 4096 8192 16384
+  do
+    time=$(grep "took" log-emilia-NPI${np} | cut -d " " -f3)
+    ite=$(grep "Iterations =" log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
+    resi=$(grep "Norm =" log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
 
-  echo "${np},${time},${ite},${resi}" >> emilia_strong_logs.csv
+    echo "${np},${n},${time},${ite},${resi}" >> weak_logs.csv
+  done
 done
 
 echo "Done !"
---
+++ log/log_parser_emilia.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# rm all_logs_Dense seq_logs_Dense.csv MPI_logs_Dense.csv
+# rm all_logs_Sparse seq_logs_Sparse.csv MPI_logs_Sparse.csv
+rm strong/emilia_strong_logs.csv
+
+echo "np,time,ite,resi" >> strong/emilia_strong_logs.csv
+for np in 1 2 4 6 8 12 16 20 24 28 32 36 40 48 56
+do
+  time=$(grep "took" strong/log-emilia-NPI${np} | cut -d " " -f3)
+  ite=$(grep "Iterations =" strong/log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
+  resi=$(grep "Norm =" strong/log-emilia-NPI${np} | cut -d "=" -f2 | cut -d " " -f2)
+
+  echo "${np},${time},${ite},${resi}" >> strong/emilia_strong_logs.csv
+done
+
+echo "Done !"
