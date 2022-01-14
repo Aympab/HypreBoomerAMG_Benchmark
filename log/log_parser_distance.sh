2b46b97d451cd3d209b58f2362d43e361cfb1a55
Aympab <aymeric.millan@mines-paristech.fr>
2022-01-15 00:34:43 +0100

    rapport

--- log/log_parser_distance.sh
+++ log/log_parser_distance.sh
@@ -2,20 +2,20 @@
 # rm all_logs_Dense seq_logs_Dense.csv MPI_logs_Dense.csv
 # rm all_logs_Sparse seq_logs_Sparse.csv MPI_logs_Sparse.csv
 
-logfile=node_distance/dist_np2_nx1024.csv
+logfile=strong/aggressive_level/agglevel_np28_nx8096.csv
 rm $logfile
 
-echo "dist,time,ite,resi" >> $logfile
+echo "paramvalue,time,ite,resi" >> $logfile
 
-for dist in 0 1 2 3
+for param in 0 1 2 3 4 5 7 8 9 10 11 12 16 20
 do
-  filename=node_distance/log-distance-${dist}
+  filename=strong/aggressive_level/log-agglevel${param}
 
   time=$(grep "took" ${filename} | cut -d " " -f3)
   ite=$(grep "Iterations =" ${filename} | cut -d "=" -f2 | cut -d " " -f2)
   resi=$(grep "Norm =" ${filename} | cut -d "=" -f2 | cut -d " " -f2)
 
-  echo "${dist},${time},${ite},${resi}" >> $logfile
+  echo "${param},${time},${ite},${resi}" >> $logfile
 done
 
 echo "Done !"
